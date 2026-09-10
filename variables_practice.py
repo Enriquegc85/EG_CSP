304494f69dc4fd31a59a6dc5646e54512d4beb67
Enriquegc85 <enrique.gomez@ucas-edu.net>
2026-09-10 13:50:07 -0600

    variables_practice.py

---
+++ variables_practice.py
@@ -0,0 +1,21 @@
+#EG, variables_practice.py
+name = "Enrique"
+print("Hello my name is", name)
+number_1to10 = "9"
+print("My lucky number is", number_1to10)
+number_100to1000 = "113"
+print("and", number_100to1000)
+breakfast = "eggs and bacon"
+print("For breakfast I ate", breakfast)
+color = "green"
+print("My favprite color is", color)
+school_name = "UCAS"
+print("I attend", school_name)
+year = "2026"
+print("The year is", year)
+eye_color = "brown"
+print("My eye color is", eye_color)
+age = "15"
+print("Iam", age)
+subject = "Computer Science"
+print("My favorite sunject is", subject)
