32ba7997a48e75d1c6cb59a78450d8dbad7facd7
Enriquegc85 <enrique.gomez@ucas-edu.net>
2026-09-14 14:50:32 -0600

    Memory_diagram.

---
+++ Memory_diagram.py
@@ -0,0 +1,20 @@
+#EG, Memory Diagram Activity
+
+name = "Enrique"
+print(f"The variable name has a value of {name} and is saved at {id(name)}")
+age = 15
+print(f"The variable age has a value of {age} and is saved at {id(age)}")
+score = 10
+print(f"The variable score has a value of {score} and is saved at {id(score)}")
+score = 20
+print(f"The variable score has a value of {score} and is saved at {id(score)}")
+height = 5.9
+print(f"The variable height has a value of {height} and is saved at {id(height)}")
+city = "Vineyard"
+print(f"The variable city has a value of {city} and is saved at {id(city)}")
+zipcode = 84059
+print(f"The variable zipcode has a value of {zipcode} and is saved at {id(zipcode)}")
+price1 = 10
+print(f"The variable price1 has a value of {price1} and is saved at {id(price1)}")
+price2 = 30
+print(f"The variable price2 has a value of {price2} and is saved at {id(price2)}")
