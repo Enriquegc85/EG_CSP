ba78dc20e8f034990601309da485f46bbb2eda4a
Enriquegc85 <enrique.gomez@ucas-edu.net>
2026-09-24 11:17:48 -0600

    Update Password-strength_ checker.py

--- Password-strength_ checker.py
+++ Password-strength_ checker.py
@@ -1,31 +1,75 @@
-#EG, Password Strength Checker
-
-
+# EG - Password Strength Checker
 
 password = input("Enter a password: ")
 
-charecters = False
+characters = False
 upercase = False
 lowercase = False
-number = False 
+number = False
 symbol = False
 
 for letter in password:
     if letter.isupper():
         upercase = True
-    elif letter.islower():
-        lowercase = True
-    elif letter.isnumeric():
-        number = True
-    else:
-        symbol = True
-        characters = True
-    if len(password) < 8:
-        characters = False
 
-if upercase and lowercase and number and symbol and characters:
-    print("Your password is strong")
-if upercase and lowercase and number and symbol and not characters:
-    print("Your password is medium")
-if upercase and lowercase and number and not symbol and not characters:
-    print("Your password is weak")
+    if letter.islower():
+        lowercase = True
+
+    if letter.isnumeric():
+        number = True
+
+    if not letter.isalnum():
+        symbol = True
+
+if len(password) >= 8:
+    characters = True
+
+score = 0
+
+if characters:
+    score = score + 1
+
+if upercase:
+    score = score + 1
+
+if lowercase:
+    score = score + 1
+
+if number:
+    score = score + 1
+
+if symbol:
+    score = score + 1
+
+print("At least 8 characters:", characters)
+print("Has an uppercase letter:", upercase)
+print("Has a lowercase letter:", lowercase)
+print("Has a number:", number)
+print("Has a symbol:", symbol)
+
+if score == 5:
+    print("Your password strength is: Strong")
+
+if score == 3 or score == 4:
+    print("Your password strength is: Medium")
+
+if score <= 2:
+    print("Your password strength is: Weak")
+
+if score < 5:
+    print("To make it Strong, add:")
+
+    if not characters:
+        print("- At least 8 characters")
+
+    if not upercase:
+        print("- An uppercase letter")
+
+    if not lowercase:
+        print("- A number")
+
+    if not number:
+        print("- A number")
+
+    if not symbol:
+        print("- A symbol")
