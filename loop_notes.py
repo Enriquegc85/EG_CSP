146ba53c4ec7d5dc1bcb892dcbacce65b47ff00d
Enriquegc85 <enrique.gomez@ucas-edu.net>
2026-09-24 14:53:23 -0600

    loop

--- loop_notes.py
+++ loop_notes.py
@@ -1,1 +1,21 @@
 #EG, loop notes
+import random
+
+
+count = 1
+while count <= 10:
+    print(count)
+    count += 1
+
+ducks = 1
+goose = random.randint(1,11)
+while True:
+    if ducks == goose:
+        break
+    print("Duck. . . .")
+    ducks += 1 #ducks = ducks + 1
+print("GOOSE!!!!")
+
+# complex data type holds other data types, like strings, integers, floats, and even other lists
+siblings = ["Marisol" , "Maribel" , "Fernando"]
+print(siblings[0])
