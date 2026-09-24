5b39f8c36986776f446aa8367b36d1ac8bf8639c
Enriquegc85 <enrique.gomez@ucas-edu.net>
2026-09-24 14:15:37 -0600

    password

--- Password-strength_ checker.py
+++ Password-strength_ checker.py
@@ -25,51 +25,33 @@ if len(password) >= 8:
     characters = True
 
 score = 0
+if characters == True:
+    score += 1
+if upercase == True:
+    score += 1
+if lowercase == True:
+    score += 1
+if number == True:
+    score += 1
+if symbol == True:
+    score += 1
 
-if characters:
-    score = score + 1
-
-if upercase:
-    score = score + 1
-
-if lowercase:
-    score = score + 1
-
-if number:
-    score = score + 1
-
-if symbol:
-    score = score + 1
-
-print("At least 8 characters:", characters)
-print("Has an uppercase letter:", upercase)
-print("Has a lowercase letter:", lowercase)
-print("Has a number:", number)
-print("Has a symbol:", symbol)
+print(f"At least 8 characters:", {characters})
+print(f"Has an uppercase letter:", {upercase})
+print(f"Has a lowercase letter:", {lowercase})
+print(f"Has a number:", {number})
+print(f"Has a symbol:", {symbol})
+print(f"Password strength score:", {score})
 
 if score == 5:
-    print("Your password strength is: Strong")
+    print("Your password is very strong.")
+if score == 4:
+    print("Your password is strong.")
+if score == 3:
+    print("Your password is moderate.")
+if score == 2:
+    print("Your password is weak.")
+if score == 1:
+    print("Your password is very weak.")
 
-if score == 3 or score == 4:
-    print("Your password strength is: Medium")
 
-if score <= 2:
-    print("Your password strength is: Weak")
-
-if score < 5:
-    print("To make it Strong, add:")
-
-    if not characters:
-        print("- At least 8 characters")
-
-    if not upercase:
-        print("- An uppercase letter")
-
-    if not lowercase:
-        print("- A number")
-
-    if not number:
-        print("- A number")
-
-    if not symbol:
-        print("- A symbol")
---
+++ loop_notes.py
@@ -0,0 +1,1 @@
+#EG, loop notes
